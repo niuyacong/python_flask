42f73c5fdbe15a5cdf561681addc6960bac3a190
niuyacong <1139800084@qq.com>
2019-12-07 17:18:18 +0800

    table-test

--- venv/projects/build/js/custom.js
+++ venv/projects/build/js/custom.js
@@ -208,14 +208,14 @@ $(document).ready(function() {
 
 // iCheck
 $(document).ready(function() {
-    if ($("input.flat")[0]) {
-        $(document).ready(function () {
+    // if ($("input.flat")[0]) {
+    //     $(document).ready(function () {
             $('input.flat').iCheck({
                 checkboxClass: 'icheckbox_flat-green',
                 radioClass: 'iradio_flat-green'
             });
-        });
-    }
+    //     });
+    // }
 });
 // /iCheck
 
